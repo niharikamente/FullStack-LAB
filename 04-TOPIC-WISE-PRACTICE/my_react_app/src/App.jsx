7321535c467f2cc864043d9d489ad05a225ec068
niharikamente <menteniharika.24.csm@anits.edu.in>
2026-09-22 12:11:24 +0530

    feat: update student information

--- 04-TOPIC-WISE-PRACTICE/my_react_app/src/App.jsx
+++ 04-TOPIC-WISE-PRACTICE/my_react_app/src/App.jsx
@@ -2,9 +2,14 @@ import studentImage from "./assets/student.png";
 import "./App.css";
 
 function App() {
+  const name = "Niharika";
+    const age = 19;
+    const department = "CSM";
+
   const imageUrl = "https://static.vecteezy.com/system/resources/thumbnails/050/894/408/small/young-woman-teacher-teaching-cartoon-character-illustration-of-teacher-explaining-in-front-of-the-board-free-vector.jpg";
+  
   function showMessage() {
-        alert("Button clicked!");
+        alert("Welcome " + name);
     }
 
   return (
@@ -12,9 +17,9 @@ function App() {
       <h1 className="heading">Welcome to React</h1>
 
       <h2>Student Information</h2>
-      <p>Name: Neha</p>
-      <p>Department: CSE</p>
-      <p>Year: 3rd</p>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Department: {department}</p>
       <img src={studentImage} alt="Student" width="200" height="200" />
       <img src={imageUrl} alt="Teacher" width="200" height="200" />
       <button onClick={showMessage}>Click Me</button>
